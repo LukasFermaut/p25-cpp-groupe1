0d422352c0d65aafd159308fab0c7208e78df7a9
Fermaut <lukas.fermaut@etu.minesparis.psl>
2026-01-11 10:40:17 +0100

    d

--- stackclass.cpp
+++ stackclass.cpp
@@ -7,19 +7,22 @@ private:
     int* tab;
 public:
     Stack (int s): nb(0){
-        ~if (s<=0){
+        if (s<=0){
             throw std::runtime_error("Stack bad size");
         }
         size=s;
         tab=new int[size];} 
     bool is_full(){
-        return nb>=size
+        return nb>=size;
     }
     bool is_empty(){
-        return nb>=size
+        return nb==0;
     }
     void push(int v){
-        if(not is_full){
+        if(is_full()){ 
+            throw std::overflow_error("Pile pleine");
+        }
+        if(not is_full()){
             tab[nb]=v;
             nb+=1;
         }
@@ -44,8 +47,28 @@ public:
         }
         return *this;
     }
-
-};
+    int pop(){
+    if (is_empty())
+        throw std::overflow_error("pile vide");
+    nb=nb-1;
+    return tab[nb - 1];
+    }
+    int top(){
+    if (is_empty())
+        throw std::underflow_error("pile vide");
+    return tab[nb - 1];
+    }
+    void print(){
+   std::cout<< '[';
+    for(int i=0; i<nb; i++){
+        std::cout<< tab[i];
+        if(i<nb-1){ std::cout<<',';}
+    }
+    std::cout<< '[' <<std::endl;
+    }
 ~Stack(){
     delete [] tab;
 }
+};
+
+
